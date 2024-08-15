140e5f691e0496bed2847053c2dd63813f18b2f4
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2024-08-15 16:19:40 -0300

    Update UpdateUser.sql

--- Sql/Procedure/UpdateUser.sql
+++ Sql/Procedure/UpdateUser.sql
@@ -1,13 +1,27 @@
-CREATE PROCEDURE UpdateUser
+USE [DB_NAME]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[Mottu_Procedure_Users_Update]
     @UserId INT,
     @Username NVARCHAR(50),
     @PasswordHash NVARCHAR(255),
     @ProfileId INT
 AS
 BEGIN
-    UPDATE Users
+    SET NOCOUNT ON;
+
+    UPDATE [dbo].[Mottu_Table_Users]
     SET Username = @Username,
         PasswordHash = @PasswordHash,
         ProfileId = @ProfileId
     WHERE Id = @UserId;
 END;
+GO
+
+
