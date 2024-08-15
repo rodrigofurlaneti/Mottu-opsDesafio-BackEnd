cbb6c588a924c8894e97f41ab4a57401b3031120
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2024-08-15 16:12:38 -0300

    Update InsertUser.sql

--- Sql/Procedure/InsertUser.sql
+++ Sql/Procedure/InsertUser.sql
@@ -1,11 +1,25 @@
-CREATE PROCEDURE InsertUser
+USE [DB_NAME]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[Mottu_Procedure_Users_Insert]
     @Username NVARCHAR(50),
     @PasswordHash NVARCHAR(255),
     @ProfileId INT
 AS
 BEGIN
-    INSERT INTO Users (Username, PasswordHash, ProfileId)
+    SET NOCOUNT ON;
+
+    INSERT INTO [dbo].[Mottu_Table_Users] (Username, PasswordHash, ProfileId)
     VALUES (@Username, @PasswordHash, @ProfileId);
     
     SELECT SCOPE_IDENTITY() AS NewUserId;
 END;
+GO
+
+
