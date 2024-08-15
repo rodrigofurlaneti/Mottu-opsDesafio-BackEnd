55f4ebffd1316a534b8361e6108502b678fe9bc6
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2024-08-15 16:01:11 -0300

    Update GetUserById.sql

--- Sql/Procedure/GetUserById.sql
+++ Sql/Procedure/GetUserById.sql
@@ -1,8 +1,22 @@
-CREATE PROCEDURE GetUserById
-    @UserId INT
+USE [DB_NAME]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[Mottu_Procedure_Users_GetById]
+    @UsersId INT
 AS
 BEGIN
-    SELECT Id, Username, ProfileId, CreatedAt
-    FROM Users
-    WHERE Id = @UserId;
+    SET NOCOUNT ON;
+
+    SELECT *
+    FROM [dbo].[Mottu_Table_Users]
+    WHERE Id = @UsersId;
 END;
+GO
+
+
