518635567c9402466c76e915070c51735e5e5b62
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2024-08-15 16:08:55 -0300

    Update InsertMotorcycle.sql

--- Sql/Procedure/InsertMotorcycle.sql
+++ Sql/Procedure/InsertMotorcycle.sql
@@ -1,12 +1,26 @@
-CREATE PROCEDURE InsertMotorcycle
+USE [DB_NAME]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[Mottu_Procedure_Motorcycles_Insert]
     @Identifier NVARCHAR(50),
     @Year INT,
     @Model NVARCHAR(100),
     @LicensePlate NVARCHAR(10)
 AS
 BEGIN
-    INSERT INTO Motorcycles (Identifier, Year, Model, LicensePlate)
+    SET NOCOUNT ON;
+
+    INSERT INTO [dbo].[Mottu_Table_Motorcycles] (Identifier, Year, Model, LicensePlate)
     VALUES (@Identifier, @Year, @Model, @LicensePlate);
     
     SELECT SCOPE_IDENTITY() AS NewMotorcycleId;
 END;
+GO
+
+
