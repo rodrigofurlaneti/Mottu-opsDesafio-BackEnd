bfd08188638e6777628275b9a1780fab1060e29b
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2024-08-15 16:07:11 -0300

    Update InsertCourier.sql

--- Sql/Procedure/InsertCourier.sql
+++ Sql/Procedure/InsertCourier.sql
@@ -1,4 +1,13 @@
-CREATE PROCEDURE InsertCourier
+USE [rodrigofurlaneti3108_Finance]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[Mottu_Procedure_Couriers_Insert]
     @Identifier NVARCHAR(50),
     @Name NVARCHAR(100),
     @CNPJ NVARCHAR(14),
@@ -8,8 +17,13 @@ CREATE PROCEDURE InsertCourier
     @CNHImagePath NVARCHAR(255) = NULL
 AS
 BEGIN
-    INSERT INTO Couriers (Identifier, Name, CNPJ, BirthDate, CNHNumber, CNHType, CNHImagePath)
+    SET NOCOUNT ON;
+
+    INSERT INTO [dbo].[Mottu_Table_Couriers] (Identifier, Name, CNPJ, BirthDate, CNHNumber, CNHType, CNHImagePath)
     VALUES (@Identifier, @Name, @CNPJ, @BirthDate, @CNHNumber, @CNHType, @CNHImagePath);
     
     SELECT SCOPE_IDENTITY() AS NewCourierId;
 END;
+GO
+
+
