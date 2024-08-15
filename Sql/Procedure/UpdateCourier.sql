63f57ec204f91a485cb30d615871770b0edcf1a4
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2024-08-15 16:16:22 -0300

    Update UpdateCourier.sql

--- Sql/Procedure/UpdateCourier.sql
+++ Sql/Procedure/UpdateCourier.sql
@@ -1,4 +1,13 @@
-CREATE PROCEDURE UpdateCourier
+USE [DB_NAME]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[Mottu_Procedure_Couriers_Update]
     @CourierId INT,
     @Identifier NVARCHAR(50),
     @Name NVARCHAR(100),
@@ -9,7 +18,9 @@ CREATE PROCEDURE UpdateCourier
     @CNHImagePath NVARCHAR(255) = NULL
 AS
 BEGIN
-    UPDATE Couriers
+    SET NOCOUNT ON;
+
+    UPDATE [dbo].[Mottu_Table_Couriers]
     SET Identifier = @Identifier,
         Name = @Name,
         CNPJ = @CNPJ,
@@ -19,3 +30,6 @@ BEGIN
         CNHImagePath = @CNHImagePath
     WHERE Id = @CourierId;
 END;
+GO
+
+
