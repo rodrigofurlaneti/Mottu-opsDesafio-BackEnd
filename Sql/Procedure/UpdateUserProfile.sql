7cfa64b6dd5b774acbceb23a5471f8395ea19680
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2024-08-15 16:20:38 -0300

    Update UpdateUserProfile.sql

--- Sql/Procedure/UpdateUserProfile.sql
+++ Sql/Procedure/UpdateUserProfile.sql
@@ -1,9 +1,23 @@
-CREATE PROCEDURE UpdateUserProfile
+USE [rodrigofurlaneti3108_Finance]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[Mottu_Procedure_UserProfiles_Update]
     @ProfileId INT,
     @ProfileName NVARCHAR(50)
 AS
 BEGIN
-    UPDATE UserProfiles
+    SET NOCOUNT ON;
+
+    UPDATE [dbo].[Mottu_Table_UserProfiles]
     SET ProfileName = @ProfileName
     WHERE Id = @ProfileId;
 END;
+GO
+
+
