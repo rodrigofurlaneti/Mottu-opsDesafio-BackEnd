227665223f03f61006cf78045fbe93ccf9337af2
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2024-08-15 15:35:33 -0300

    Update DeleteUser.sql

--- Sql/Procedure/DeleteUser.sql
+++ Sql/Procedure/DeleteUser.sql
@@ -1,7 +1,19 @@
-CREATE PROCEDURE DeleteUser
-    @UserId INT
+USE [DB_NAME]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[Mottu_Procedure_User_Delete] (@UserId INT)
+
 AS
-BEGIN
-    DELETE FROM Users
-    WHERE Id = @UserId;
-END;
+BEGIN TRANSACTION
+    DELETE FROM [dbo].[Mottu_Table_User]
+		WHERE Id = @UserId;
+COMMIT TRANSACTION
+GO
+
+
