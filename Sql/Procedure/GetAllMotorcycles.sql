1b887ddfabd7027d62835ed8ee192cd67b56c998
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2024-08-15 15:44:34 -0300

    Update GetAllMotorcycles.sql

--- Sql/Procedure/GetAllMotorcycles.sql
+++ Sql/Procedure/GetAllMotorcycles.sql
@@ -1,6 +1,19 @@
-CREATE PROCEDURE GetAllMotorcycles
+USE [DB_NAME]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[Mottu_Procedure_Motorcycles_GetAll]
 AS
 BEGIN
-    SELECT Id, Identifier, Year, Model, LicensePlate, RegistrationDate
-    FROM Motorcycles;
+    SET NOCOUNT ON;
+
+    SELECT 
+		*
+    FROM [dbo].[Mottu_Table_Motorcycles];
 END;
+GO
