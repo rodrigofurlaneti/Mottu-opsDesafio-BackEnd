29bf8e047b8002e00b660f5beb1f722f3626b59e
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2024-08-15 16:14:14 -0300

    Update InsertUserProfile.sql

--- Sql/Procedure/InsertUserProfile.sql
+++ Sql/Procedure/InsertUserProfile.sql
@@ -1,9 +1,23 @@
-CREATE PROCEDURE InsertUserProfile
+USE [DB_NAME]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[Mottu_Procedure_UserProfiles_Insert]
     @ProfileName NVARCHAR(50)
 AS
 BEGIN
-    INSERT INTO UserProfiles (ProfileName)
+    SET NOCOUNT ON;
+
+    INSERT INTO [dbo].[Mottu_Table_UserProfiles] (ProfileName)
     VALUES (@ProfileName);
     
     SELECT SCOPE_IDENTITY() AS NewProfileId;
 END;
+GO
+
+
