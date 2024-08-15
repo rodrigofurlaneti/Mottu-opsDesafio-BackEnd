a67a169753c5b4920672fa1fe879491d505dadf7
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2024-08-15 15:43:56 -0300

    Update GetAllUserProfiles.sql

--- Sql/Procedure/GetAllUserProfiles.sql
+++ Sql/Procedure/GetAllUserProfiles.sql
@@ -1,6 +1,21 @@
-CREATE PROCEDURE GetAllUserProfiles
+USE [DB_NAME]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[Mottu_Procedure_UserProfiles_GetAll]
 AS
 BEGIN
-    SELECT Id, ProfileName
-    FROM UserProfiles;
+    SET NOCOUNT ON;
+
+    SELECT 
+		*
+    FROM [dbo].[Mottu_Table_UserProfiles];
 END;
+GO
+
+
