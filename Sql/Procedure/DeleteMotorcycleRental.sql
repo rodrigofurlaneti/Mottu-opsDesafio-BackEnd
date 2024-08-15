fee201c757e68fed42946c3cfd38fd36c10e6c8c
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2024-08-15 15:36:14 -0300

    Update DeleteMotorcycleRental.sql

--- Sql/Procedure/DeleteMotorcycleRental.sql
+++ Sql/Procedure/DeleteMotorcycleRental.sql
@@ -1,7 +1,19 @@
-CREATE PROCEDURE DeleteMotorcycleRental
-    @RentalId INT
+USE [DB_NAME]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[Mottu_Procedure_MotorcycleRental_Delete] (@MotorcycleRentalId INT)
+
 AS
-BEGIN
-    DELETE FROM MotorcycleRentals
-    WHERE Id = @RentalId;
-END;
+BEGIN TRANSACTION
+    DELETE FROM [dbo].[Mottu_Table_MotorcycleRental]
+		WHERE Id = @MotorcycleRentalId;
+COMMIT TRANSACTION
+GO
+
+
