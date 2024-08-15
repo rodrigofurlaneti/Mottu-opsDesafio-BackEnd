4c8de97b0d55c8f20fc4e403b200d846f179b50d
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2024-08-15 15:53:38 -0300

    Update GetAllUsers.sql

--- Sql/Procedure/GetAllUsers.sql
+++ Sql/Procedure/GetAllUsers.sql
@@ -1,6 +1,21 @@
-CREATE PROCEDURE GetAllUsers
+USE [DB_NAME]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[Mottu_Procedure_Users_GetAll]
 AS
 BEGIN
-    SELECT Id, Username, ProfileId, CreatedAt
-    FROM Users;
+    SET NOCOUNT ON;
+
+    SELECT 
+		*
+    FROM [dbo].[Mottu_Table_Users];
 END;
+GO
+
+
